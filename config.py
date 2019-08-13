26d022894c972e8fbea2ca1cee4d5a178dee1637
whutKarson <whutkarson@163.com>
2019-08-13 23:18:36 +0800

    Chapter3-Chapter5
    Chapter3-Chapter5

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # for SQLite database configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ test.py
@@ -0,0 +1,8 @@
+from app import db
+from app.models import User, Post
+
+# test query
+
+
+
+
